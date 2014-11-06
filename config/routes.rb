e69dd300826a02e25b107552153576487175d5f9
ANISHA BHANDARI <anishabhandari@ANISHAs-MacBook-Pro.local>
2014-11-05 19:43:43 -0500

    set root path, changed URLs

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   resources :users
 
   get 'welcome/index'
-get 'welcome/contact'
-  get 'welcome/about'
+
+  get 'all_about_us' => 'welcome#about'
+  
+  get 'drop_us_a_line' => 'welcome#contact'
+
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
